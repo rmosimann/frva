392d8d082abc7fc004cfc2df43aa6152540633d0
Roland Mosimann <roland.mosimann@student.fhnw.ch>
2017-09-21 15:49:00 +0200

    Changed presenter to controller

--- frva/src/main/java/FrvaApp.java
+++ frva/src/main/java/FrvaApp.java
@@ -1,5 +1,3 @@
-import presenter.Parser;
-
 public class FrvaApp {
   public static void main(String[] args) {
 
--- frva/src/main/java/model/Parser.java
+++ frva/src/main/java/model/Parser.java
@@ -1,4 +1,4 @@
-package presenter;
+package model;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
