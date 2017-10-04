a0e18da9ed1a4267c03bd0e92691c56b87efb088
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2017-10-04 22:07:25 +0200

    First shot on a MainView

--- frva/src/main/java/FrvaApp.java
+++ frva/src/main/java/FrvaApp.java
@@ -1,5 +1,19 @@
-public class FrvaApp {
-  public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class FrvaApp extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("view/mainView.fxml"));
+    primaryStage.setTitle("ColorPicker");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.show();
   }
 }
