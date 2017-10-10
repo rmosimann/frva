9d87cf33a795d41f50aca802add168374507e4e4
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2017-10-10 22:49:50 +0200

    Made the Graph work

--- frva/src/main/java/controller/TabController.java
+++ frva/src/main/java/controller/TabController.java
@@ -1,12 +1,14 @@
 package controller;
 
+import java.util.Map;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import model.FrvaModel;
+
 
 public class TabController {
   private final FrvaModel model;
@@ -19,11 +21,7 @@ public class TabController {
   @FXML
   private LineChart<Double, Double> datachart;
 
-  @FXML
-  private NumberAxis xaxis;
 
-  @FXML
-  private NumberAxis yaxis;
 
   @FXML
   private void initialize() {
@@ -32,28 +30,51 @@ public class TabController {
 
   private void addDataToGraph() {
 
+    double[] calibration = model.getLibrary()
+        .get(0).getWavelengthCalibrationFile().getCalibration();
+    Map<String, double[]> measurements = model.getLibrary()
+        .get(0).getDataFiles().get(0).getMeasureSequences().get(0).getMeasurements();
+
+    double[] data = measurements.get("VEG");
     LineChart.Series<Double, Double> series1 = new LineChart.Series<Double, Double>();
-    series1.setName("Series 1");
-    series1.getData().add(new XYChart.Data<Double, Double>(0.0, 1.0));
-    series1.getData().add(new XYChart.Data<Double, Double>(1.2, 1.4));
-    series1.getData().add(new XYChart.Data<Double, Double>(2.2, 1.9));
-    series1.getData().add(new XYChart.Data<Double, Double>(2.7, 2.3));
-    series1.getData().add(new XYChart.Data<Double, Double>(2.9, 0.5));
+    series1.setName("VEG");
+    for (int i = 0; i < 1023; i++) {
+      series1.getData().add(new XYChart.Data<>(calibration[i], data[i]));
+    }
 
     ObservableList<XYChart.Series<Double, Double>> lineChartData
         = FXCollections.observableArrayList();
 
     lineChartData.add(series1);
 
-    LineChart.Series<Double, Double> series2 = new LineChart.Series<Double, Double>();
-    series2.setName("Series 2");
-    series2.getData().add(new XYChart.Data<>(0.0, 1.6));
-    series2.getData().add(new XYChart.Data<Double, Double>(0.8, 0.4));
-    series2.getData().add(new XYChart.Data<Double, Double>(1.4, 2.9));
-    series2.getData().add(new XYChart.Data<Double, Double>(2.1, 1.3));
-    series2.getData().add(new XYChart.Data<Double, Double>(2.6, 0.9));
+    data = measurements.get("WR");
+    series1 = new LineChart.Series<Double, Double>();
+    series1.setName("WR");
+    for (int i = 0; i < 1023; i++) {
+      series1.getData().add(new XYChart.Data<>(calibration[i], data[i]));
+    }
 
-    lineChartData.add(series2);
+    lineChartData.add(series1);
+
+
+    data = measurements.get("DC_WR");
+    series1 = new LineChart.Series<Double, Double>();
+    series1.setName("DC_WR");
+    for (int i = 0; i < 1023; i++) {
+      series1.getData().add(new XYChart.Data<>(calibration[i], data[i]));
+    }
+
+    lineChartData.add(series1);
+
+    data = measurements.get("DC_VEG");
+    series1 = new LineChart.Series<Double, Double>();
+    series1.setName("DC_VEG");
+    for (int i = 0; i < 1023; i++) {
+      series1.getData().add(new XYChart.Data<>(calibration[i], data[i]));
+    }
+
+    lineChartData.add(series1);
+
 
     datachart.setData(lineChartData);
   }
--- frva/src/main/java/model/FrvaModel.java
+++ frva/src/main/java/model/FrvaModel.java
@@ -1,10 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.data.SdCard;
+
 public class FrvaModel {
   private final String applicationName = "FRVA";
+
+  private final List<SdCard> library = new ArrayList<>();
+
+  public FrvaModel() {
+    library.add(new SdCard(getClass().getResource("../SDCARD")));
+  }
 
   public String getApplicationName() {
     return applicationName;
   }
 
+  public List<SdCard> getLibrary() {
+    return library;
+  }
 }
