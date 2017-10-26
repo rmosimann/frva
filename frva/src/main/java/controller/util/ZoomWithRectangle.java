e844582e5cebd0f81bfaa8b3d2bdebb606c17b91
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2017-10-26 13:02:05 +0200

    Formated ZoomRect

--- frva/src/main/java/controller/util/ZoomWithRectangle.java
+++ frva/src/main/java/controller/util/ZoomWithRectangle.java
@@ -159,6 +159,7 @@ public class ZoomWithRectangle implements ZoomLineChart {
     }
   }
 
+
   private void addListeners() {
 
     areAxisAutoranging.bind(xaxis.autoRangingProperty().and(yaxis.autoRangingProperty()));
@@ -212,6 +213,7 @@ public class ZoomWithRectangle implements ZoomLineChart {
         if (Mode.ZOOMIN.equals(currentMouseMode.getValue())) {
           zoomStartPoint = new Point2D(event.getX(), event.getY());
           zoomRect = new Rectangle(event.getX(), event.getY(), 0, 0);
+          zoomRect.getStyleClass().add("zoomRect");
           anchorPane.getChildren().add(zoomRect);
         }
       }
