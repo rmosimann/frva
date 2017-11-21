e42b4a6f643e2182c47a6a03e2b7e9553612fa60
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2017-11-21 08:59:19 +0100

    Implementing StatePattern

--- frva/src/main/java/controller/LiveViewController.java
+++ frva/src/main/java/controller/LiveViewController.java
@@ -1,28 +1,42 @@
 package controller;
 
+import controller.util.bluetooth.BluetoothConnection;
 import controller.util.bluetooth.ConnectionState;
-import controller.util.bluetooth.ConnectionStateBltOff;
 import controller.util.bluetooth.ConnectionStateInit;
 import java.util.List;
+import java.util.logging.Logger;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javax.bluetooth.ServiceRecord;
 import model.FrvaModel;
 
 
 public class LiveViewController {
+  private final Logger logger = Logger.getLogger("FRVA");
   private final FrvaModel model;
   private Node activeView;
+  ConnectionStateInit connectionStateInit;
 
   List<ServiceRecord[]> availableServiceRecords;
 
-  private ConnectionState state;
-  private ConnectionState bltoff = new ConnectionStateBltOff(this);
-  private ConnectionState searching;
-  private ConnectionState initState = new ConnectionStateInit(this);
+  private ObjectProperty<ConnectionState> state = new SimpleObjectProperty<>();
+
+  @FXML
+  private LineChart<?, ?> datachart;
+
+  @FXML
+  private NumberAxis xaxis;
+
+  @FXML
+  private NumberAxis yaxis;
 
   @FXML
   private HBox messageBOxOutgreyHbox;
@@ -46,10 +60,14 @@ public class LiveViewController {
   private Label messageBoxTitleSearchingLabel;
 
   @FXML
-  private HBox messageBoxConnectToHbox;
+  private HBox messageBoxAvailableDevicesHbox;
 
   @FXML
   private Label messageBoxTitleSelectLabel;
+
+  @FXML
+  private VBox AvailableDevicesListVbox;
+
 
   /**
    * Constructor.
@@ -57,16 +75,22 @@ public class LiveViewController {
    * @param model the one and only model.
    */
   public LiveViewController(FrvaModel model) {
-    state = initState;
+
+    connectionStateInit = new ConnectionStateInit(this);
+    state.setValue(connectionStateInit);
     this.model = model;
     addListeners();
   }
 
   private void addListeners() {
     model.activeViewProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue.equals(activeView)) {
-        state.handle();
+      if (newValue.equals(activeView) && state.getValue().equals(connectionStateInit)) {
+        state.getValue().handle();
       }
+    });
+
+    state.addListener(observable -> {
+      state.getValue().handle();
     });
   }
 
@@ -78,7 +102,7 @@ public class LiveViewController {
   public void displayBluetoothOffDialog(boolean active) {
     messageBOxOutgreyHbox.setVisible(active);
     messageBoxHbox.setVisible(active);
-    cancelButton.setOnAction(event -> state.handle());
+    cancelButton.setOnAction(event -> state.getValue().handle());
   }
 
 
@@ -91,7 +115,7 @@ public class LiveViewController {
   }
 
   public void setState(ConnectionState state) {
-    this.state = state;
+    this.state.setValue(state);
   }
 
   public void displaySearchingDialog(boolean active) {
@@ -106,5 +130,22 @@ public class LiveViewController {
 
   public void setAvailableServiceRecords(List<ServiceRecord[]> availableServiceRecords) {
     this.availableServiceRecords = availableServiceRecords;
+  }
+
+  Button serviceRecordButton;
+
+  public void displayAvailableDevicesDialog(boolean active) {
+
+
+    availableServiceRecords.forEach(serviceRecords -> {
+      System.out.println("mddddddddddmdmdmd" + serviceRecords);
+      serviceRecordButton = new Button();
+      serviceRecordButton.setText(serviceRecords[0].getConnectionURL(0, false));
+      serviceRecordButton.setOnAction(event -> BluetoothConnection.connectToService(serviceRecords));
+      AvailableDevicesListVbox.getChildren().add(serviceRecordButton);
+    });
+    messageBOxOutgreyHbox.setVisible(active);
+    messageBoxAvailableDevicesHbox.setVisible(active);
+
   }
 }
--- frva/src/main/java/controller/util/bluetooth/ConnectionStateSearching.java
+++ frva/src/main/java/controller/util/bluetooth/ConnectionStateSearching.java
@@ -1,6 +1,7 @@
 package controller.util.bluetooth;
 
 import controller.LiveViewController;
+import javafx.concurrent.Task;
 
 public class ConnectionStateSearching implements ConnectionState {
   private final LiveViewController liveViewController;
@@ -12,12 +13,29 @@ public class ConnectionStateSearching implements ConnectionState {
   @Override
   public void handle() {
     liveViewController.displaySearchingDialog(true);
+
     try {
-      liveViewController.setAvailableServiceRecords(
-          BluetoothConnection.getDevicesWithSerialService());
+      Task<Void> bla = new Task<Void>() {
+        @Override
+        protected Void call() throws Exception {
+          liveViewController.setAvailableServiceRecords(
+              BluetoothConnection.getDevicesWithSerialService());
+          return null;
+        }
+      };
+      bla.setOnSucceeded(event -> {
+            liveViewController.displaySearchingDialog(false);
+            liveViewController.setState(new ConnectionStateAvailableDevices(liveViewController));
+          }
+      );
+
+
+      Thread thread = new Thread(bla);
+      thread.setDaemon(true);
+      thread.start();
+
     } catch (Exception e) {
       e.printStackTrace();
     }
-    liveViewController.displaySearchingDialog(false);
   }
 }
