bfee4480b877537fdcc002ee96356b7b090fcd6d
Roland Mosimann <roland.mosimann@students.fhnw.ch>
2017-12-05 20:25:47 +0100

    Renamed Commands, implementing that LiveViewLineChart, updating with the current measurement

---
+++ frva/src/main/java/controller/util/liveviewparser/CommandSetInterval.java
@@ -0,0 +1,42 @@
+package controller.util.liveviewparser;
+
+import model.FrvaModel;
+
+public class CommandSetInterval extends AbstractCommand {
+  StringBuilder stringBuilder = new StringBuilder();
+  private int intervalTime;
+
+
+  public CommandSetInterval(LiveDataParser liveDataParser, FrvaModel model, int intervalTime) {
+    super(liveDataParser, model);
+    this.intervalTime = intervalTime;
+  }
+
+
+  @Override
+  public void sendCommand() {
+    liveDataParser.executeCommand(Commands.i.toString() + " "
+        + String.valueOf(intervalTime));
+  }
+
+
+  @Override
+  public void receive(char read) {
+    stringBuilder.append((char) read);
+
+    if (stringBuilder.toString().contains(System.lineSeparator())) {
+      handleLine(stringBuilder);
+      stringBuilder = new StringBuilder();
+    }
+  }
+
+
+  private void handleLine(StringBuilder stringBuilder) {
+    if (stringBuilder.toString().contains("Interval[s] = ")) {
+      liveDataParser.getDeviceStatus().setIntervalTime(parseNumber(stringBuilder.toString()));
+    }
+    if (stringBuilder.toString().contains("config.txt written")) {
+      liveDataParser.runNextCommand();
+    }
+  }
+}
