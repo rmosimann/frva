173f6f29506871201157f5c6b53858a52bb7bf42
Patrick Wigger <pwigger@gmail.com>
2017-12-07 13:40:05 +0100

    userfriendly name

--- frva/src/main/java/controller/LiveViewController.java
+++ frva/src/main/java/controller/LiveViewController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.logging.Logger;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
@@ -31,6 +32,7 @@ public class LiveViewController {
   private final FrvaModel model;
   private Node activeView;
   private ConnectionStateInit connectionStateInit;
+  private String deviceName;
 
   private List<ServiceRecord[]> availableServiceRecords;
   private ServiceRecord[] selectedServiceRecord;
@@ -122,23 +124,54 @@ public class LiveViewController {
         msgBoxDevicesList.getChildren().clear();
       }
       availableServiceRecords.forEach(serviceRecords -> {
-        String deviceAddress = serviceRecords[0].getHostDevice().getBluetoothAddress();
-        String deviceName = deviceAddress;
-        deviceName = serviceRecords[0].getHostDevice().getBluetoothAddress();
+        final String[] deviceAddress = {serviceRecords[0].getHostDevice().getBluetoothAddress()};
+        final String[] deviceName = {deviceAddress[0]};
+        Runnable r = new Runnable() {
+          @Override
+          public void run() {
+            try {
+              deviceName[0] = serviceRecords[0].getHostDevice().getFriendlyName(false);
+
+            } catch (IOException e) {
+
+              e.printStackTrace();
+            }
+          }
+        };
+
+        Thread t = new Thread(r);
+        t.start();
+
+
+
 
         VBox buttonLabaleBox = new VBox();
-        Label namelabel = new Label(deviceName);
+
+        Label namelabel=null;
+
+        try {
+          t.join();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        namelabel = new Label(deviceName[0]);
+
+
         namelabel.setStyle("-fx-font-weight: bold");
-        String[] split = deviceAddress.split("(?<=\\G..)");
+
+
+        String[] split = deviceAddress[0].split("(?<=\\G..)");
         Label addresslabel = new Label(Arrays.toString(split)
             .replace(", ", ":"));
         buttonLabaleBox.getChildren().addAll(namelabel, addresslabel);
+
+
         Button serviceRecordButton = new Button();
         serviceRecordButton.setGraphic(buttonLabaleBox);
         serviceRecordButton.setPrefWidth(200);
         serviceRecordButton.setPrefHeight(50);
 
-        serviceRecordButton.setOnAction(event -> {
+        serviceRecordButton.setOnAction((ActionEvent event) -> {
           setSelectedServiceRecord(serviceRecords);
           displayAvailableDevicesDialog(false);
           setState(new ConnectionStateConnecting(this));
@@ -197,6 +230,7 @@ public class LiveViewController {
 
   /**
    * Just for testing reasons.
+   *
    * @param openStreamConnection The Stream of the device
    */
   public void setOpenStreamConnection(StreamConnection openStreamConnection) {
